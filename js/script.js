18dea5f04ff23614260660d031f349622345ad22
amihijumeee <syahmi3003@gmail.com>
2026-07-22 18:26:27 +0800

    chef kecik

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const hamburger =
+    document.querySelector(".hamburger");
+
+
+const menu =
+    document.querySelector(".nav-links");
+
+
+hamburger.addEventListener("click", () => {
+
+    menu.classList.toggle("active"); F
+
+});
